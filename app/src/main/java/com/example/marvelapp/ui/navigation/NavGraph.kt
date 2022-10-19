f48a4ae021063dbc94a0d81d2a6ad27c8a1f0ce8
joNA <jitzian@gmail.com>
2022-10-19 07:07:01 -0400

    marvel jc - Working on navigation graph. Adding logic to receive params

--- app/src/main/java/com/example/marvelapp/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/marvelapp/ui/navigation/NavGraph.kt
@@ -2,11 +2,14 @@ package com.example.marvelapp.ui.navigation
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.example.marvelapp.ui.screens.characters.views.CharactersScreenState
-import com.example.marvelapp.ui.screens.comics.views.ComicsScreen
+import com.example.marvelapp.ui.screens.comics.views.ComicsScreenState
 
 @ExperimentalFoundationApi
 @Composable
@@ -15,11 +18,42 @@ fun NavGraph(navController: NavHostController) {
         navController = navController,
         startDestination = Screens.CharactersScreen.route
     ) {
-        composable(route = Screens.CharactersScreen.route) {
-            CharactersScreenState(navController)
-        }
-        composable(route = Screens.ComicsScreen.route) {
-            ComicsScreen(navController)
-        }
+        addCharactersScreen(navController, this)
+        addComicsScreen(navController, this)
+    }
+}
+
+@ExperimentalFoundationApi
+private fun addCharactersScreen(
+    navController: NavHostController,
+    navGraphBuilder: NavGraphBuilder
+) {
+    navGraphBuilder.composable(route = Screens.CharactersScreen.route) {
+        CharactersScreenState(
+            onBackButton = { navController.popBackStack() },
+            navigateToComics = { characterId ->
+                navController.navigate(Screens.ComicsScreen.withArgs(characterId.toString()))
+            }
+        )
+    }
+}
+
+private fun addComicsScreen(
+    navController: NavHostController,
+    navGraphBuilder: NavGraphBuilder
+) {
+    navGraphBuilder.composable(
+        route = Screens.ComicsScreen.withArgsFormat(Screens.ComicsScreen.characterId),
+        arguments = listOf(
+            navArgument(Screens.ComicsScreen.characterId) {
+                type = NavType.IntType
+            }
+        )
+    ) { navBackStackEntry ->
+        val args = navBackStackEntry.arguments
+
+        ComicsScreenState(
+            characterId = args?.getInt(Screens.ComicsScreen.characterId) ?: 0,
+            onBackButton = { navController.popBackStack() })
     }
 }
--- app/src/main/java/com/example/marvelapp/ui/navigation/Screens.kt
+++ app/src/main/java/com/example/marvelapp/ui/navigation/Screens.kt
@@ -2,5 +2,28 @@ package com.example.marvelapp.ui.navigation
 
 sealed class Screens(val route: String) {
     object CharactersScreen : Screens("characters_screen")
-    object ComicsScreen : Screens("comics_screen")
+    object ComicsScreen : Screens("comics_screen") {
+        const val characterId = "characterId"
+    }
+
+    // build navigation path (for screen navigation)
+    fun withArgs(vararg args: String): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
+    }
+
+    // build and setup route format (in navigation graph)
+    fun withArgsFormat(vararg args: String): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/{$arg}")
+            }
+        }
+    }
+
 }
