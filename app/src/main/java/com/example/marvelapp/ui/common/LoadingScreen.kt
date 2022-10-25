068eb14ab82fa26690b4b0bf9032360d527fa479
joNA <jitzian@gmail.com>
2022-10-25 11:41:07 -0400

    marvel jc - Updating reference that I've used for LoadingScreen

--- app/src/main/java/com/example/marvelapp/ui/common/LoadingScreen.kt
+++ app/src/main/java/com/example/marvelapp/ui/common/LoadingScreen.kt
@@ -18,6 +18,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.marvelapp.R
 
+/**
+ * Reference: https://www.jetpackcompose.net/jetpack-compose-progress-indicator-progressbar
+ * **/
+
 @Composable
 @Preview(showBackground = true)
 fun LoadingScreen() {
